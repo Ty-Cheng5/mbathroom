4345070c74afce0b44658ffed132cfe04a3c3441
Ty-Cheng5 <tsenopole@gmail.com>
2026-08-01 18:31:45 -0400

    got leaflet to work

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,14 +3,15 @@ import "./Navbar.css";
 function Navbar() {
   return (
     <nav className="navbar">
-      <nav className="navbar-left">
-        <a href="/">M Bathroom</a>
-      </nav>
-      <nav className="navbar-inner">
+      <a href="/" className="navbar-brand">Home</a>
+      <div className="navbar-inner">
         <a href="/Frequency">Frequency</a>
         <a href="/FActivity">Friend Activity</a>
         <a href="/Profile">Profile</a>
-      </nav>
+        <button type="button" className="navbar-cta">
+          Write a Review
+        </button>
+      </div>
     </nav>
   );
 }
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,40 @@
+import { useEffect, useRef } from 'react';
 import './Home.css';
 import Navbar from '../components/Navbar';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+
 
 
 function Home() {
+    const mapRef = useRef(null);
+
+    useEffect(() => {
+        const map = L.map(mapRef.current).setView([42.2808, -83.7430], 15); // lat, lng, zoom
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; OpenStreetMap contributors'
+        }).addTo(map);
+
+        return () => map.remove();
+    }, []);
+
     return (
         <div>
-            <Navbar />
+            <Navbar/>
+            <div className="filter-bar">
+                <h1>Search</h1>
+
+            </div>
+            <div className="rate-leaflet">
+                <div className="rate">
+
+                </div>
+                <div ref={mapRef} className="leaflet">
+
+                </div>
+
+            </div>
+
 
         </div>
     );
